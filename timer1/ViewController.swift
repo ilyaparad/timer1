c270b9508b660c83395f86910701b2dabc24a718
ilyaparad <prdxflv@mail.ru>
2022-04-12 22:46:40 +0400

    Initial Commit

---
+++ timer1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  timer1
+//
+//  Created by test on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
